c1d296d17cfe457972ea8bddee00da16feef049e
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-03-10 03:36:56 +0000

    Update Ocl.java

--- app/src/main/java/com/example/breastcancerlocal/viewModel/Ocl.java
+++ app/src/main/java/com/example/breastcancerlocal/viewModel/Ocl.java
@@ -1,4 +1,4 @@
-package com.example.cloudbreastcancer.viewModel; // Place in the relevant package directory of your application.
+package com.example.breastcancerlocal.viewModel; // Place in the relevant package directory of your application.
 
 /******************************
 * Copyright (c) 2003--2022 Kevin Lano
